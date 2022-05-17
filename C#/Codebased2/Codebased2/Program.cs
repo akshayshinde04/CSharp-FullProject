bf96dae8f762fa07d06749bf3934bd65c3feed6a
Akshay Shinde <a.shinde1@zensar.com>
2022-05-17 18:25:12 +0530

    Codebase2

---
+++ C#/Codebased2/Codebased2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace codebase2
+{ 
+public delegate void calculator(int a, int b);
+
+
+
+class Program
+{
+    public static void add(int a, int b)
+    {
+        int res = a + b;
+        Console.WriteLine("Addition result is {res}");
+    }
+    public static void sub(int a, int b)
+    {
+        int res = a - b;
+        Console.WriteLine("Substraction result is {res}");
+    }
+    public static void multi(int a, int b)
+    {
+        int res = a * b;
+        Console.WriteLine("Multiplication result is {res}");
+    }
+    public static void divi(int a, int b)
+    {
+        int res = a / b;
+        Console.WriteLine("Division result is {res}");
+    }
+    static void Main(string[] args)
+    {
+        calculator obj = new calculator(Program.add);
+        obj.Invoke(10, 20);
+        obj = sub;
+        obj(20, 10);
+        obj = multi;
+        obj(30, 40);
+        obj = divi;
+        obj(50, 10);
+        Console.ReadLine();
+    }
+    }
+}
+
+
